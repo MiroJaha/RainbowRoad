c86c697fb80867bc8b686e69c023a480c620c340
Mohammad Jaha <miro.tune@gmail.com>
2021-12-06 18:19:24 +0000

    Uploading Ass

--- RainbowRoad/ViewController.swift
+++ RainbowRoad/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let colors: [UIColor] = [.red,.orange,.yellow,.green,.blue,.purple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,3 +19,15 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        
+        return cell
+    }
+}
